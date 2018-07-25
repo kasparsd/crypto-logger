33419f8d52c47d12e0fed398f504dfb80c542110
Kaspars Dambis <hi@kaspars.net>
2018-07-25 10:44:01 +0300

    Use tabs and specify a reasonable timeout

--- src/logger.php
+++ src/logger.php
@@ -3,41 +3,47 @@
 date_default_timezone_set('UTC');
 
 $fiatSybmols = [
-  'USD',
-  'GBP',
-  'JPY',
-  'CHF',
-  'EUR',
-  'CAD',
-  'AUD',
-  'KRW',
-  'RUB',
-  'CNY',
-  'ARS',
-  'HKD',
-  'INR',
-  'SGD',
-  'AED',
+	'USD',
+	'GBP',
+	'JPY',
+	'CHF',
+	'EUR',
+	'CAD',
+	'AUD',
+	'KRW',
+	'RUB',
+	'CNY',
+	'ARS',
+	'HKD',
+	'INR',
+	'SGD',
+	'AED',
 ];
 
 $csvFields = [
-  'fromsymbol',
-  'tosymbol',
-  'price',
-  'lastupdate',
-  'lastvolume',
+	'fromsymbol',
+	'tosymbol',
+	'price',
+	'lastupdate',
+	'lastvolume',
 ];
 
 $apiUrl = sprintf(
-    'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=%s',
-    implode( ',', $fiatSybmols )
+	'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=%s',
+	implode( ',', $fiatSybmols )
 );
 
-$apiResponse = json_decode( file_get_contents( $apiUrl ) );
+$context = stream_context_create([
+	'http' => [
+		'timeout' => 10,
+	]
+]);
+
+$apiResponse = json_decode( file_get_contents( $apiUrl, false, $context ) );
 
 if ( ! isset( $apiResponse->RAW->ETH ) ) {
-    error_log( 'Failed to fetch data from the API.' );
-    die;
+	error_log( 'Failed to fetch data from the API.' );
+	die;
 }
 
 $filenameDate = date( 'Ymd' );
@@ -46,40 +52,40 @@ $csvFieldsApi = array_map( 'strtoupper', $csvFields );
 $fileMap = [];
 
 foreach ( $apiResponse->RAW->ETH as $refSymbol => $fields ) {
-    $row = [
-        time(),
-    ];
+	$row = [
+		time(),
+	];
 
-    foreach ( $csvFieldsApi as $csvFieldName ) {
-        if ( isset( $fields->$csvFieldName ) ) {
-            $row[] = $fields->$csvFieldName;
-        } else {
-            $row[] = null;
-        }
-    }
+	foreach ( $csvFieldsApi as $csvFieldName ) {
+		if ( isset( $fields->$csvFieldName ) ) {
+			$row[] = $fields->$csvFieldName;
+		} else {
+			$row[] = null;
+		}
+	}
 
-    $fileMap[] = [
-      'filename' => sprintf(
-        '%s/data/%s-eth-%s.csv',
-        dirname( __DIR__ ),
-        $filenameDate,
-        strtolower( $refSymbol )
-      ),
-      'header' => array_merge( [ 'timestamp' ], $csvFields ),
-      'data' => [ $row ],
-    ];
+	$fileMap[] = [
+		'filename' => sprintf(
+			'%s/data/%s-eth-%s.csv',
+			dirname( __DIR__ ),
+			$filenameDate,
+			strtolower( $refSymbol )
+		),
+		'header' => array_merge( [ 'timestamp' ], $csvFields ),
+		'data' => [ $row ],
+	];
 }
 
 foreach ( $fileMap as $file ) {
-  if ( ! file_exists( $file['filename'] ) ) {
-    file_put_contents( $file['filename'], implode( ',', $file['header'] ) . "\n" );
-  }
+	if ( ! file_exists( $file['filename'] ) ) {
+		file_put_contents( $file['filename'], implode( ',', $file['header'] ) . "\n" );
+	}
 
-  $rows = [];
+	$rows = [];
 
-  foreach ( $file['data'] as $rowData ) {
-    $rows[] = implode( ',', $rowData );
-  }
+	foreach ( $file['data'] as $rowData ) {
+		$rows[] = implode( ',', $rowData );
+	}
 
-  file_put_contents( $file['filename'], implode( "\n", $rows ) . "\n", FILE_APPEND );
+	file_put_contents( $file['filename'], implode( "\n", $rows ) . "\n", FILE_APPEND );
 }
